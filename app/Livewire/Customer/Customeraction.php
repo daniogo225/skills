b3da2d7682e206dfe5d3d16a06323e5d740fc2cc
daniogo <daniogozieaboubakar@gmail.com>
2025-03-25 18:39:50 +0000

    ading customer an product and display

--- app/Livewire/Customer/Customeraction.php
+++ app/Livewire/Customer/Customeraction.php
@@ -10,7 +10,6 @@ class Customeraction extends Component
     public function render()
     {
         $customers = Customer::all();
-        dd($customers);
         return view('livewire.customer.customeraction', compact('customers'));
     }
 }
